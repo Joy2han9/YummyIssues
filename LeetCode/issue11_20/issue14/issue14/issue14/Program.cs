c19db7372a947265027c81edac37eaadb63be6db
JoyZhang <Joy.Zhang@serko.com>
2020-09-10 13:23:00 +0800

    issue14

---
+++ LeetCode/issue11_20/issue14/issue14/issue14/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace issue14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0)
+            {
+                return "";
+            }
+
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+
+            var shortestStr = GetShortestStr(strs);
+            var tempStr = shortestStr;
+            while (tempStr.Length > 0)
+            {
+                bool isStart = true;
+                foreach (var str in strs)
+                {
+                    if (!str.StartsWith(tempStr))
+                    {
+                        isStart = false;
+                        break;
+                    }
+                }
+                if (isStart)
+                {
+                    break;
+                }
+                else
+                {
+                    tempStr = tempStr.Substring(0, tempStr.Length - 1);
+                }
+            }
+
+            return tempStr;
+        }
+
+        public static string GetShortestStr(string[] strs)
+        {
+            return strs.OrderBy(m => m.Length).First();
+        }
+    }
+}
