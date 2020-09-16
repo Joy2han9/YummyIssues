aad5d0bdc7493999d02d2b137bf207e05398cafd
JoyZhang <Joy.Zhang@serko.com>
2020-09-16 16:56:59 +0800

    issue27

---
+++ LeetCode/issue21_30/issue27/issue27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace issue27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = RemoveElement(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2);
+
+            Console.ReadKey();
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            var newNums = nums.Where(x => x != val).OrderBy(x=>x).ToArray();
+            for (int i = 0; i < newNums.Length; i++)
+            {
+                nums[i] = newNums[i];
+            }
+
+            return newNums.Length;
+        }        
+    }
+}
