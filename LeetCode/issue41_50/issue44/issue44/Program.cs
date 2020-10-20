23550b65bacb83af6c4118851066cf98de2da842
JoyZhang <Joy.Zhang@serko.com>
2020-10-20 15:28:03 +0800

    issue44

--- LeetCode/issue41_50/issue44/issue44/Program.cs
+++ LeetCode/issue41_50/issue44/issue44/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace issue44
@@ -53,55 +54,140 @@ namespace issue44
          */
         static void Main(string[] args)
         {
-            var result = IsMatch("abcabczzzde", "*abc???de*");
+            var result = IsMatch("abcabcabcabcasdjfkpwqueirjsdakfllkabcdede", "abc*abcabc*a?c*e");
             Console.WriteLine(result);
         }
 
         public static bool IsMatch(string s, string p)
         {
-            if (s=="")
+            int sLength = s.Length;
+            int pLength = p.Length;
+            int sIndex = 0;
+            int pIndex = 0;
+            int start = -1;
+            int match = 0;
+            while (sIndex < sLength)
             {
-                return p == "" || p.All(x => x == '*');
-            }
-            if(p.IndexOf('x')==-1 && p.Length > s.Length)
-            {
-                return false;
-            }
-
-            var tempS = s;
-            var result = true;
-            var indexS = 0;
-            for(var i = 0; i < p.Length; i++)
-            {
-                var currentP = p[i];
-                if(currentP == '*')
+                if (pIndex < pLength && (s[sIndex]== p[pIndex] || p[pIndex] == '?'))
                 {
-                    if (i == p.Length - 1)
-                    {
-                        return true;
-                    }
-                    else if (i + 1 < p.Length)
-                    {
-                        tempS = s.Substring(indexS);
-                        indexS = tempS.IndexOf(p[i + 1]) + s.Length - tempS.Length;
-                    }
+                    sIndex++;
+                    pIndex++;
                 }
-                else if (currentP == '?' || currentP == s[indexS])
+                else if (pIndex < pLength && p[pIndex] == '*')
                 {
-                    indexS++;
-                    if(indexS > s.Length)
-                    {
-                        return false;
-                    }
-                    continue;
+                    start = pIndex;
+                    match = sIndex;
+                    pIndex++;
+                }
+                else if (start != -1)
+                {
+                    pIndex = start + 1;
+                    match++;
+                    sIndex = match;
                 }
                 else
                 {
                     return false;
                 }
             }
+            while (pIndex < pLength)
+            {
+                if (p[pIndex] != '*') return false;
+                pIndex++;
+            }
+            return true;
+        }
 
-            result = indexS == s.Length;
+        public static bool IsMatch1(string s, string p)
+        {
+            if (s == "")
+            {
+                return p == "" || p.All(x => x == '*');
+            }
+            if (p.IndexOf('*') != -1 && p.Length > s.Length)
+            {
+                return false;
+            }
+
+            var startIndexList = GetSubStringPList(p);
+            var sIndex = 0;
+            var pIndex = 0;
+
+            for (; pIndex < startIndexList.Count; pIndex++)
+            {
+                var current = startIndexList[pIndex];
+                if (current == "*")
+                {
+                    if (pIndex == startIndexList.Count - 1)
+                    {
+                        return true;
+                    }
+                    var next = startIndexList[pIndex + 1];
+                    while (sIndex + next.Length <= s.Length)
+                    {
+                        if (Equal(s.Substring(sIndex, next.Length), next))
+                        {
+                            sIndex += next.Length;
+                            pIndex++;
+                            break;
+                        }
+                        else
+                        {
+                            sIndex++;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Equal(s.Substring(sIndex, current.Length), current))
+                    {
+                        sIndex += current.Length;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return sIndex == s.Length && pIndex == startIndexList.Count;
+        }
+
+        public static bool Equal(string a, string b)
+        {
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (b[i] == '?')
+                {
+                    continue;
+                }
+                else if (a[i] != b[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static List<string> GetSubStringPList(string p)
+        {
+            var result = new List<string>();
+            var pSplitByStart = p.Split('*');
+
+            for (var i = 0; i < pSplitByStart.Length - 1; i++)
+            {
+                if (pSplitByStart[i] != "")
+                {
+                    result.Add(pSplitByStart[i]);
+                }
+                result.Add("*");
+            }
+
+            if (pSplitByStart[pSplitByStart.Length - 1] != "")
+            {
+                result.Add(pSplitByStart[pSplitByStart.Length - 1]);
+            }
 
             return result;
         }
