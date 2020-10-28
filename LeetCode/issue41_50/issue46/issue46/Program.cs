6d573f179d5d0d1d2f4c2428e6eb23ae4cadb7a5
joy.zhang <joy.zhang@serko.com>
2020-10-28 11:19:02 +0800

    issue46 and issue47

---
+++ LeetCode/issue41_50/issue46/issue46/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace issue46
+{
+    class Program
+    {
+        /*
+         * 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+
+            示例:
+
+            输入: [1,2,3,4]
+            输出:
+            [
+              [1,2,3,4],
+              [1,2,4,3],
+              [1,3,2,4],
+              [1,3,4,2],
+              [1,4,3,2],
+              [1,4,2,3],
+            ]
+         */
+        static void Main(string[] args)
+        {
+            var array = new int[] { 1, 2, 3 };
+            var result = Permute(array);
+
+            Console.WriteLine("a");
+        }
+        public static IList<IList<int>> Permute(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            if (nums.Length == 0)
+            {
+                return result;
+            }
+            if (nums.Length == 1)
+            {
+                result.Add(new List<int>() { nums[0] });
+                return result;
+            }
+
+            perm(nums, 0, nums.Length - 1);
+
+            void perm(int[] list, int i, int n)
+            {
+                int j;
+
+                if (i == n)
+                {
+                    result.Add(new List<int>(list));
+                }
+                else
+                {
+                    for (j = i; j <= n; j++)
+                    {
+                        if (i == j)
+                        {
+                            perm(list, i + 1, n);
+                        }
+                        else
+                        {
+                            SWAP(ref list[i], ref list[j]);
+                            perm(list, i + 1, n);
+                            SWAP(ref list[i], ref list[j]);//数组一定要复原！！！！！                     
+                        }
+                    }
+                }
+            }
+            void SWAP(ref int a, ref int b)
+            {
+                int c = a;
+                a = b;
+                b = c;
+            }
+
+            return result;
+        }
+
+        public static IList<IList<int>> Permute1(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            if (nums.Length == 0)
+            {
+                return result;
+            }
+            if (nums.Length == 1)
+            {
+                result.Add(new List<int>() { nums[0] });
+                return result;
+            }
+            var numsLength = nums.Length;
+
+            var count = 1;
+            while (numsLength > 0)
+            {
+                count *= numsLength;
+                numsLength--;
+            }
+
+            for (var i = 0; i < count; i++)
+            {
+                var time = count / nums.Length;
+                result.Add(new List<int>() { i / time });
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/issue41_50/issue47/issue47/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace issue47
+{
+    class Program
+    {
+        /*
+         * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+
+            示例:
+
+            输入: [1,1,2]
+            输出:
+            [
+              [1,1,2],
+              [1,2,1],
+              [2,1,1]
+            ]
+         */
+        private bool[] vis;
+        static void Main(string[] args)
+        {
+            var result = PermuteUnique(new int[] { 1, 1, 2 });
+        }
+
+        public IList<IList<int>> PermuteUnique(int[] nums)
+        {
+            var ans = new List<IList<int>>();
+            var perm = new List<int>();
+            vis = new bool[nums.Length];
+            Array.Sort(nums);
+            backtrack(nums, ans, perm);
+            return ans;
+        }
+
+        public void backtrack(int[] nums, List<IList<int>> ans, List<int> perm)
+        {
+            if (perm.Count == nums.Length)
+            {
+                ans.Add(new List<int>(perm));
+                return;
+            }
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1]))
+                {
+                    continue;
+                }
+                perm.Add(nums[i]);
+                vis[i] = true;
+                backtrack(nums, ans, perm);
+                vis[i] = false;
+                perm.RemoveAt(perm.Count - 1);
+            }
+        }
+
+        public static IList<IList<int>> PermuteUnique1(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            var strResult = new List<string>();
+
+            perm(nums, 0, nums.Length - 1);
+
+            void perm(int[] list, int i, int n)
+            {
+                int j;
+
+                if (i == n)
+                {
+                    var tempStr = GetStrValueFromIntArray(list);
+                    if (!strResult.Contains(tempStr))
+                    {
+                        strResult.Add(tempStr);
+                        result.Add(new List<int>(list));
+                    }
+                }
+                else
+                {
+                    for (j = i; j <= n; j++)
+                    {
+                        if (i == j || list[i] == list[j])
+                        {
+                            perm(list, i + 1, n);
+                        }
+                        else
+                        {
+                            SWAP(ref list[i], ref list[j]);
+                            perm(list, i + 1, n);
+                            SWAP(ref list[i], ref list[j]);//数组一定要复原！！！！！                     
+                        }                            
+                    }
+                }
+            }
+            void SWAP(ref int a, ref int b)
+            {
+                int c = a;
+                a = b;
+                b = c;
+            }
+
+            return result;
+        }
+
+        public static string GetStrValueFromIntArray(int[] array)
+        {
+            var stringBuilder = new StringBuilder();
+            for(var i = 0; i < array.Length; i++)
+            {
+                stringBuilder.Append(array[i]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
