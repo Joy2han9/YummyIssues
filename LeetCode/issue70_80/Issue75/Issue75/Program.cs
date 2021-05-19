d25016c27438fb0d68b73b86438e1758a54188bc
joy.zhang <joy.zhang@serko.com>
2021-05-19 15:30:00 +0800

    issue75

---
+++ LeetCode/issue70_80/Issue75/Issue75/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+
+namespace Issue75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myArray = new int[] { 2,2,2,2,2,1,2,1,2,1 };
+            DateTime d1 = DateTime.Now;
+            SortColors2(myArray);
+            Console.WriteLine((DateTime.Now - d1).TotalMilliseconds);
+        }
+
+        public static void SortColors(int[] nums)
+        {
+            int zeroCount=0, oneCount=0, twoCount=0;
+            zeroCount = nums.Count(x => x == 0);
+            oneCount = nums.Count(x => x == 1);
+            twoCount = nums.Count(x => x == 2);
+          
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i<zeroCount)
+                {
+                    nums[i] = 0;
+                    continue;
+                }
+
+                if (i>=zeroCount && i< zeroCount + oneCount)
+                {
+                    nums[i] = 1;
+                    continue;
+                }
+
+                if (i>= zeroCount + oneCount && i <nums.Length)
+                {
+                    nums[i] = 2;
+                    continue;
+                }
+            }
+        }
+
+        public static void SortColors1(int[] nums)
+        {
+            Array.Sort(nums);
+        }
+
+        public static void SortColors2(int[] nums)
+        {
+            int low = 0, high = nums.Length - 1, index = 0, temp;
+
+            while (index <= high)
+            {
+
+                if (nums[index] == 0)
+                {
+                    temp = nums[index];
+                    nums[index] = nums[low];
+                    nums[low] = temp;
+                    low++;
+                    index = low;
+
+                }
+                else
+                {
+                    if (nums[index] == 2)
+                    {
+                        temp = nums[index];
+                        nums[index] = nums[high];
+                        nums[high] = temp;
+                        high--;
+
+                    }
+                }
+                if (index >= nums.Length - 1) break;
+                if (nums[index] == 1) index++;
+
+
+            }
+
+        }
+
+        
+    }
+}
---
+++ LeetCode/issue70_80/issue74/issue74/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace issue74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new int[][] { 
+                new int []{1},
+            };
+
+            SearchMatrix(matrix, 3);
+        }
+
+        public static bool SearchMatrix(int[][] matrix, int target)
+        {
+            var width = matrix.Length;
+            var deepth = matrix[0].Length;
+            var length = width * deepth;
+          
+            var myArray = new int[length];
+            var index = 0;
+            for(var i = 0; i < width; i++)
+            {
+                for(var j = 0; j < deepth; j++)
+                {
+                    myArray[index++] = matrix[i][j];
+                }
+            }
+
+            var result = -1;
+            result = BinarySearch(myArray, 0, length, target);
+            return result > -1;
+        }
+
+        public static int BinarySearch(int[] arr, int low, int high, int key)
+        {
+            int mid = (low + high) / 2;
+            if (mid >= arr.Length) return -1;
+            if (low > high)
+            {
+                return -1;
+            }                
+            else
+            {
+                if (arr[mid] == key)
+                    return mid;
+                else if (arr[mid] > key)
+                    return BinarySearch(arr, low, mid - 1, key);
+                else
+                    return BinarySearch(arr, mid + 1, high, key);
+            }
+        }
+    }
+}
