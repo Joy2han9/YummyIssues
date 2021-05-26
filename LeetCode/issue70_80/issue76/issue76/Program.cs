5661344a676876fb6017539a964aff8df8096db3
joy.zhang <joy.zhang@serko.com>
2021-05-26 11:12:20 +0800

    issue 76 and 77

---
+++ LeetCode/issue70_80/issue76/issue76/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace issue76
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = "obzcopzocynyrsgsarijyxnkpnukkrvzuwdjldxndmnvevpgmxrmvfwkutwekrffnloyqnntbdohyfqndhzyoykiripdzwiojyoznbtogjyfpouuxvumtewmmnqnkadvzrvouqfbbdiqremqzgevkbhyoznacqwbhtrcjwfkzpdstpjswnpiqxjhywjanhdwavajrhwtwzlrqwmombxcaijzevbtcfsdcuovckoalcseaesmhrrizcjgxkbartdtotpsefsrjmvksqyahpijsrppdqpvmuocofuunonybjivbjviyftsyiicbzxnwnrmvlgkzticetyfcvqcbjvbufdxgcmesdqnowzpshuwcseenwjqhgsdlxatamysrohfnixfprdsljyyfhrnnjsagtuihuczilgvtfcjwgdhpbixlzmakebszxbhrdibpoxiwztshwczamwnninzmqrmpsviydkptjzpktksrortapgpxwojofxeasoyvyprjoguhqobehugwdvtzlenrcttuitsiijswpogicjolfxhiscjggzzissfcnxnvgftxvbfzkukqrtalvktdjsodmtgzqtuyaqvvrbuexgwqzwduixzrpnvegddyyywaquxjxrnuzlmyipuqotkghfkpknqinoidifnfyczzonxydtqroazxhjnrxfbmtlqcsfhshjrxwqvblovaouxwempdrrplefnxmwrwfjtebrfnfanvvmtbzjesctdgbsfnpxlwihalyiafincfcwgdfkvhebphtxukwgjgplrntsuchyjjuqozakiglangxkttsczhnswjksnuqwflmumpexxrznzwxurrysaokwxxqkrggytvsgkyfjrewrcvntomnoazmzycjrjrqemimyhriyxgrzcfuqtjhvjtuhwfzhwpljzajitrhryaqchnuawbxhxrpvyqcvhpggrpplhychyulijhkglinibedauhvdydkqszdbzfkzbvhldstocgydnbfjkcnkfxcyyfbzmmyojgzmasccaahpdnzproaxnexnkamwmkmwslksfpwirexxtymkmojztgmfhydvlqtddewjvsrmyqjrpycbmndhupmdqqabiuelacuvxnhxgtpvrtwfgzpcrbhhtikbcqpctlxszgpfbgcsbaaiapmtsucocmpecgixshrrnhyrpalralbccnxvjzjllarqhznzghswqsnfuyywmzbopyjyauknxddgdthlabjqtwxpxwljvoxkpjjpfvccyikbbrpdsyvlxscuoofkecwtnfkvcnzbxkeabtdusyhrqklhaqreupakxkfzxgawqfwsaboszvlshwzhosojjotgyagygguzntrouhiweuomqptfjjqsxlbylhwtpssdlltgubczxslqjgxuqnmpynnlwjgmebrpokxjnbiltvbebyytnnjlcwyzignmhedwqbfdepqakrelrdfesqrumptwwgifmmbepiktxavhuavlfaqxqhreznbvvlakzeoomykkzftthoemqwliednfsqcnbexbimrvkdhllcesrlhhjsspvfupxwdybablotibypmjutclgjurbmhztboqatrdwsomnxnmocvixxvfiqwmednahdqhxjkvcyhpxxdmzzuyyqdjibvmfkmonfxmohhshpkhmntnoplphqyprveyfsmsxjfosmicdsjrieeytpnbhlsziwxnpmgoxneqbnufhfwrjbqcsdfarybzwaplmxckkgclvwqdbpumsmqkswmjwnkuqbicykoisqwoootrdpdvcuiuswfqmrkctsgrevcxnyncmivsxbpbxzxpwchiwtkroqisnmrbmefbmatmdknaklpgpyqlsccgunaibsloyqpnsibwuowebomrmcegejozypjzjunjmeygozcjqbnrpakdermjcckartbcppmbtkhkmmtcngteigjnxxyzaibtdcwutkvpwezisskfaeljmxyjwykwglqlnofhycwuivdbnpintuyhtyqpwaoelgpbuwiuyeqhbvkqlsfgmeoheexbhnhutxvnvfjwlzfmvpcghiowocdsjcvqrdmkcizxnivbianfpsnzabxqecinhgfyjrjlbikrrgsbgfgyxtzzwwpayapfgueroncpxogouyrdgzdfucfrywtywjeefkvtzxlwmrniselyeodysirqflpduvibfdvedgcrzpzrunpadvawfsmmddqzaaahfxlifobffbyzqqbtlcpquedzjvykvarayfldvmkapjcfzfbmhscdwhciecsbdledspgpdtsteuafzbrjuvmsfrajtulwirzagiqjdiehefmfifocadxfuxrpsemavncdxuoaetjkavqicgndjkkfhbvbhjdcygfwcwyhpirrfjziqonbyxhibelinpllxsjzoiifscwzlyjdmwhnuovvugfhvquuleuzmehggdfubpzolgbhwyeqekzccuypaspozwuhbzbdqdtejuniuuyagackubauvriwneeqfhtwkocuipcelcfrcjcymcuktegiikyosumeioatfcxrheklookaqekljtvtdwhxsteajevpjviqzudnjnqbucnfvkybggaybebljwcstmktgnipdyrxbgewqczzkaxmeazpzbjsntltjwlmuclxirwytvxgvxscztryubtjweehapvxrguzzsatozzjytnamfyiitreyxmanhzeqwgpoikcjlokebksgkaqetverjegqgkicsyqcktmwjwakivtsxjwrgakphqincqrxqhzbcnxljzwturmsaklhnvyungjrxaonjqomdnxpnvihmwzphkyuhwqwdboabepmwgyatyrgtboiypxfavbjtrgwswyvcqhzwibpisydtmltbkydhznbsvxktyfxopwkxzbftzknnwipghuoijrbgqnzovxckvojvsqqraffwowfvqvfcmiicwitrhxdeombgesxexedlakitfovtydxunqnwqqdeeekiwjnwoshqcsljiicgobbbuqakjdonjawgjlezdnqhfdqnmsuavxdpnfzwipmspiabveaarshzwxmirgkmfncvtdrdvfxkpxlkdokxgtwcskmjryyymcthfnkasinihaunohkxaibtsqelockaefjmsuolebtnepauwmrxutspjwaxbmahsjtkfkxlnszribmeofbkyvbjscjtqjakuwvcgunvnonvqbbggfshauqsyznokqbhowjusypfnecffenojfvlblgzntqzlrgzprvhqnpfrrkzxznieiuivajivzijsqijigtatifmbplzqahuidegfoobpymkputzamzvweiyvvzlwihgmmmrcburbgbsdxrfjsbiylitghgcpqjbevvgypxcybubyoijijrhuzcdijfybqbfowlookqmlnplbxvjjosfqviygqyhgamuwzjklbyzopkrnhbywtfoqomweldmlrhjqswctubiknzzvcztyehouvnyiqnvkufaobehxhrjvtisxjlxoumipzjarwvbsaegdkpbsjmpevjbewzuqnfhoohhmdjgfpmjzdmtmykqvtucptwfidpwtwffzolffzqfdearclkyeecuzabjeqhxpmfodsvisnpxrqowdawheydfyhoexvcmihdlzavtqlshdhdgjzpozvvackebhgqppvcrvymljfvooauxcjnbejdivikcoaugxwzsulgfqdtefpehbrlhaoqxwcancuvbqutnfbuygoemditeagmcveatgaikwflozgdhkyfqmjcruyyuemwbqwxyyfiwnvlmbovlmccaoguieu";
+
+
+
+            var target = "cjgamyzjwxrgwedhsexosmswogckohesskteksqgrjonnrwhywxqkqmywqjlxnfrayykqotkzhxmbwvzstrcjfchvluvbaobymlrcgbbqaprwlsqglsrqvynitklvzmvlamqipryqjpmwhdcsxtkutyfoiqljfhxftnnjgmbpdplnuphuksoestuckgopnlwiyltezuwdmhsgzzajtrpnkkswsglhrjprxlvwftbtdtacvclotdcepuahcootzfkwqhtydwrgqrilwvbpadvpzwybmowluikmsfkvbebrxletigjjlealczoqnnejvowptikumnokysfjyoskvsxztnqhcwsamopfzablnrxokdxktrwqjvqfjimneenqvdxufahsshiemfofwlyiionrybfchuucxtyctixlpfrbngiltgtbwivujcyrwutwnuajcxwtfowuuefpnzqljnitpgkobfkqzkzdkwwpksjgzqvoplbzzjuqqgetlojnblslhpatjlzkbuathcuilqzdwfyhwkwxvpicgkxrxweaqevziriwhjzdqanmkljfatjifgaccefukavvsfrbqshhswtchfjkausgaukeapanswimbznstubmswqadckewemzbwdbogogcysfxhzreafwxxwczigwpuvqtathgkpkijqiqrzwugtr";
+            var start = DateTime.Now;
+            var result = MinWindow(source, target);
+            Console.WriteLine((DateTime.Now - start).TotalMilliseconds);
+        }
+
+
+        public static string MinWindow1(string s, string t)
+        {
+            var tArray = t.ToArray();
+            Array.Sort(tArray);
+            t = new string(tArray);
+
+            var length = t.Length;
+            var index = 0;
+            var result = "";
+            var tDic = t.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count(m => m == x.Key));
+            while (index + length <= s.Length)
+            {
+                var temp = s.Substring(index, length);
+                if (CanGetMinWindow(temp, tDic))
+                {
+                    result = temp;
+                    break;
+                }
+                if (index + length < s.Length)
+                {
+                    index++;
+                }
+                else
+                {
+                    index = 0;
+                    length++;
+                }
+            }
+
+            return result;
+        }
+
+        public static string MinWindow2(string s, string t)
+        {
+            var left = 0;
+            var right = t.Length;
+            var result = "";
+            var currentLenth = s.Length;
+            var tDic = t.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count(m => m == x.Key));
+            while (right<=s.Length && right - left >= t.Length)
+            {
+                var temp = s.Substring(left, right - left);
+                if (CanGetMinWindow(temp, tDic))
+                {
+                    if(temp.Length <= currentLenth)
+                    {
+                        currentLenth = temp.Length;
+                        result = temp;
+                    }
+                    left++;
+                }
+                else
+                {
+                    right++;
+                }
+            }
+
+            return result;
+        }
+
+        public static string MinWindow(string s, string t)
+        {
+            //dicT<char, List<int>>, char: char in t, List<int>{char count, location 1, location 2, ... }
+            Dictionary<char, List<int>> dicT = new Dictionary<char, List<int>>();
+            string res = s + "end";
+
+            for (var i = 0; i < t.Length; i++)
+            {
+                if (!dicT.ContainsKey(t[i]))
+                    dicT.Add(t[i], new List<int>() { 1 });
+                else
+                    dicT[t[i]][0] += 1;
+            }
+
+
+            for (var j = 0; j < s.Length; j++)
+            {
+                if (dicT.ContainsKey(s[j]))
+                {
+                    dicT[s[j]].Add(j);
+
+                    var fullhead = j;
+                    foreach (var item in dicT)
+                    {
+                        if (item.Value.Count() <= item.Value[0])
+                        {
+                            fullhead = -1;
+                            break;
+                        }
+                        else
+                            fullhead = Math.Min(item.Value[item.Value.Count() - item.Value[0]], fullhead);
+                    }
+
+                    if (fullhead != -1 && res.Length > j - fullhead + 1)
+                        res = s.Substring(fullhead, j - fullhead + 1);
+                }
+            }
+
+            if (res.Length <= s.Length)
+                return res;
+            else
+                return "";
+        }
+
+        private static bool CanGetMinWindow(string s, Dictionary<char, int> tDic)
+        {
+            var result = true;
+
+            var sDic = s.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count(m => m == x.Key));
+
+
+
+            if (sDic.Keys.Count < tDic.Keys.Count)
+            {
+                return false;
+            }
+
+            foreach (var tKey in tDic.Keys)
+            {
+                if (!sDic.Keys.Contains(tKey) || sDic[tKey] < tDic[tKey])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/issue70_80/issue77/issue77/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace issue77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 4;
+            var k = 2;
+            var result = Combine(n, k);
+            Console.ReadKey();
+        }
+
+        public static IList<IList<int>> Combine(int n, int k)
+        {
+            var myList = new List<int>();
+            for(var i = 1; i <= n; i++)
+            {
+                myList.Add(i);
+            }
+            var result = GetCombination(myList, k);
+
+            return result;
+        }
+
+        public static List<IList<int>> GetCombination(List<int> t, int n)
+        {
+            if (t.Count < n)
+            {
+                return null;
+            }
+            int[] temp = new int[n];
+            List<IList<int>> list = new List<IList<int>>();
+            GetCombination(ref list, t, t.Count, n, temp, n);
+            return list;
+        }
+
+        public static void GetCombination(ref List<IList<int>> result, List<int> list, int n, int m, int[] b, int M)
+        {
+            for (int i = n; i >= m; i--)
+            {
+                b[m - 1] = i;
+                if (m > 1)
+                {
+                    GetCombination(ref result, list, i - 1, m - 1, b, M);
+                }
+                else
+                {
+                    if (result == null)
+                    {
+                        result = new List<IList<int>>();
+                    }
+                    List<int> temp = new List<int>();
+                    temp.AddRange(b);
+                    result.Add(temp);
+                }
+            }
+        }
+    }
+}
