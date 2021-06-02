dedccfbcf1406b25d8efc4b5ffc609e59bf3a56e
joy.zhang <joy.zhang@serko.com>
2021-06-02 17:02:36 +0800

    Issue80

---
+++ LeetCode/issue70_80/Issue80/Issue80/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Issue80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[]
+            {
+                0, 0, 1, 1, 1, 1, 2, 3, 3
+            };
+            var start = DateTime.Now;
+            var result = RemoveDuplicates(nums);
+            Console.WriteLine((DateTime.Now - start).TotalMilliseconds);
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            var diff = 0;
+            var sameCount = 1;
+            var previous = nums[0];
+            for(var i = 1; i < nums.Length; i++)
+            {
+                if(previous == nums[i])
+                {
+                    if (++sameCount > 2)
+                    {
+                        diff++;
+                    }
+                }
+                else
+                {
+                    previous = nums[i];
+                    sameCount = 1;
+                }
+                nums[i - diff] = nums[i];
+            }
+
+            return nums.Length - diff;
+        }
+    }
+}
