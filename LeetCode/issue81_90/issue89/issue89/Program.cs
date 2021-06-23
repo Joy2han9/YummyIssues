282580027d3e62dd7785ee6a16d0a3c5cb9ec91c
joy.zhang <joy.zhang@serko.com>
2021-06-23 15:52:49 +0800

    issue90

---
+++ LeetCode/issue81_90/issue89/issue89/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace issue89
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static IList<int> GrayCode(int n)
+        {
+            return null;
+        }
+    }
+}
---
+++ LeetCode/issue81_90/issue90/issue90/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace issue90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1,2,2 };
+            var result = SubsetsWithDup(arr);
+            
+        }
+
+        public static IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            var result = new List<IList<int>>()
+            {
+                new List<int>(),
+            };
+
+            var list = new List<string>();
+            foreach (int s in nums)
+            {
+                List<IList<int>> lst = result.GetRange(0, result.Count);
+                List<int> nArr = new List<int>() { s };
+                foreach (List<int> ss in lst)
+                {
+                    var temp = ss.Concat(nArr).OrderBy(x => x).ToList();
+                    var tempStr = string.Join(',', temp);
+                    if (list.Contains(string.Join(',',temp)))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        list.Add(tempStr);
+                        result.Add(temp);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
